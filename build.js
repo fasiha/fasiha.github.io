7ab16ea5003b2df442bbda127122cb830c2d3915
Ahmed Fasih <wuzzyview@gmail.com>
2023-12-23 21:13:20 -0800

    prettier

--- build.js
+++ build.js
@@ -1,37 +1,39 @@
 "use strict";
 
 var jsdom = require("jsdom");
-var {JSDOM} = jsdom;
+var { JSDOM } = jsdom;
 
 var Promise = require("bluebird");
 
-var path = require('path');
-var yaml = require('js-yaml');
-var Feed = require('feed');
-var find = require('find');
-var flatten = require('lodash.flatten');
-var fs = Promise.promisifyAll(require('fs'));
-var child_process = Promise.promisifyAll(require('child_process'));
+var path = require("path");
+var yaml = require("js-yaml");
+var Feed = require("feed");
+var find = require("find");
+var flatten = require("lodash.flatten");
+var fs = Promise.promisifyAll(require("fs"));
+var child_process = Promise.promisifyAll(require("child_process"));
 var spawnAsync = child_process.spawnAsync;
 var spawn = child_process.spawn;
 var readFileAsync = fs.readFileAsync;
 var writeFileAsync = fs.writeFileAsync;
 var appendFileAsync = fs.appendFileAsync;
-var spawnPromise = require('./spawn-promise.js');
-var spawnSync = require('child_process').spawnSync;
+var spawnPromise = require("./spawn-promise.js");
+var spawnSync = require("child_process").spawnSync;
 
-var svgOverlay = require('./svgOverlay');
+var svgOverlay = require("./svgOverlay");
 
-var URL = 'https://fasiha.github.io';
-var PREPATH = ''; // i.e., if site lives at foo.edu/~me, RELPATH='~me'.
+var URL = "https://fasiha.github.io";
+var PREPATH = ""; // i.e., if site lives at foo.edu/~me, RELPATH='~me'.
 function filepathToAbspath(filepath) {
-  return path.resolve('/', PREPATH, filepath).replace(/index.html$/, '');
+  return path.resolve("/", PREPATH, filepath).replace(/index.html$/, "");
 }
-function filepathToURL(filepath) { return URL + filepathToAbspath(filepath); }
+function filepathToURL(filepath) {
+  return URL + filepathToAbspath(filepath);
+}
 
-var plotlyPreamble = `<script src="${
-    filepathToAbspath(
-        'assets/plotly-basic-1.27.1.min.js')}" charset="utf-8"></script>\n`;
+var plotlyPreamble = `<script src="${filepathToAbspath(
+  "assets/plotly-basic-1.27.1.min.js"
+)}" charset="utf-8"></script>\n`;
 
 var mathjaxPreamble = `<script type="text/x-mathjax-config">
 MathJax.Hub.Config({
@@ -43,22 +45,23 @@ MathJax.Hub.Config({
 </script>
 `;
 
-var hl = fs.readFileSync('assets/highlight.pack.js', 'utf8');
-var hlcss = '<style>' +
-            fs.readFileSync('assets/tomorrow-night-eighties.css', 'utf8') +
-            '</style>\n';
+var hl = fs.readFileSync("assets/highlight.pack.js", "utf8");
+var hlcss =
+  "<style>" +
+  fs.readFileSync("assets/tomorrow-night-eighties.css", "utf8") +
+  "</style>\n";
 
 var defaultMeta = {
-  title : 'Insight≠Numbers',
-  description : 'A blog by Ahmed Fasih',
-  date : new Date(),
-  dateUpdated : null,
-  banner : 'glen-helen.jpg',
-  socialBanner : '',
-  tags : [],
-  author : 'Ahmed Fasih',
-  plotly : false,
-  mathjax : false,
+  title: "Insight≠Numbers",
+  description: "A blog by Ahmed Fasih",
+  date: new Date(),
+  dateUpdated: null,
+  banner: "glen-helen.jpg",
+  socialBanner: "",
+  tags: [],
+  author: "Ahmed Fasih",
+  plotly: false,
+  mathjax: false,
 };
 
 var articles = new Map([]);
@@ -69,60 +72,67 @@ function gatherIntel(filepath) {
 }
 
 function build() {
-  find.file(/\.md$/, '.', md => {
-    md = md.filter(s => s.indexOf('node_modules') !== 0);
+  find.file(/\.md$/, ".", (md) => {
+    md = md.filter((s) => s.indexOf("node_modules") !== 0);
     md.forEach(gatherIntel);
-    var metas = Array.from(articles.values())
-                    .sort((a, b) => -((a.dateUpdated || a.date) -
-                                      (b.dateUpdated || b.date)));
+    var metas = Array.from(articles.values()).sort(
+      (a, b) => -((a.dateUpdated || a.date) - (b.dateUpdated || b.date))
+    );
 
-    var postMetas = metas.filter(meta => meta.filepath.indexOf('post/') === 0);
-    var nonpostMetas =
-        metas.filter(meta => meta.filepath.indexOf('post/') !== 0);
+    var postMetas = metas.filter(
+      (meta) => meta.filepath.indexOf("post/") === 0
+    );
+    var nonpostMetas = metas.filter(
+      (meta) => meta.filepath.indexOf("post/") !== 0
+    );
 
-    var allTags =
-        Array.from(new Set(flatten(postMetas.map(meta => meta.tags)))).sort();
+    var allTags = Array.from(
+      new Set(flatten(postMetas.map((meta) => meta.tags)))
+    ).sort();
 
     postMetas.forEach((meta, midx) => {
-      buildOneMarkdown(meta, postMetas[midx + 1], postMetas[midx - 1],
-                       postMetas);
+      buildOneMarkdown(
+        meta,
+        postMetas[midx + 1],
+        postMetas[midx - 1],
+        postMetas
+      );
     });
-    nonpostMetas.forEach(
-        (meta, midx) => { buildOneMarkdown(meta, null, null, postMetas); });
+    nonpostMetas.forEach((meta, midx) => {
+      buildOneMarkdown(meta, null, null, postMetas);
+    });
 
     // Atom feed!
     var feed = new Feed({
-      title : defaultMeta.title,
-      description : defaultMeta.description,
-      id : filepathToURL(''),
-      link : filepathToURL(''),
-      image : filepathToURL(defaultMeta.banner),
-      copyright :
-          'Unless otherwise noted, released into the public domain under CC0.',
-      updated : new Date(postMetas[0].dateUpdated || postMetas[0].date),
-      author : {name : defaultMeta.author, link : filepathToURL(`#contact`)}
+      title: defaultMeta.title,
+      description: defaultMeta.description,
+      id: filepathToURL(""),
+      link: filepathToURL(""),
+      image: filepathToURL(defaultMeta.banner),
+      copyright:
+        "Unless otherwise noted, released into the public domain under CC0.",
+      updated: new Date(postMetas[0].dateUpdated || postMetas[0].date),
+      author: { name: defaultMeta.author, link: filepathToURL(`#contact`) },
     });
-    postMetas.forEach(post => {
+    postMetas.forEach((post) => {
       feed.addItem({
-        title : post.title,
-        id : filepathToURL(post.outfile),
-        link : filepathToURL(post.outfile),
-        description : post.description,
-        author : [
-          {name : post.author, link : `${filepathToURL('')}/#contact`},
-        ],
-        contributor : [],
-        date : post.dateUpdated || post.date,
-        image : post.socialBanner || post.banner
+        title: post.title,
+        id: filepathToURL(post.outfile),
+        link: filepathToURL(post.outfile),
+        description: post.description,
+        author: [{ name: post.author, link: `${filepathToURL("")}/#contact` }],
+        contributor: [],
+        date: post.dateUpdated || post.date,
+        image: post.socialBanner || post.banner,
       });
     });
-    allTags.forEach(tag => feed.addCategory(tag));
-    fs.writeFileSync('atom.xml', feed.atom1());
+    allTags.forEach((tag) => feed.addCategory(tag));
+    fs.writeFileSync("atom.xml", feed.atom1());
   });
 }
 
 function preprocessMarkdown(markdown, meta) {
-  var dir = meta.filepath.split('/').slice(0, -1).join('/');
+  var dir = meta.filepath.split("/").slice(0, -1).join("/");
   return svgOverlay.markdownToHtml(markdown, dir);
 }
 
@@ -131,36 +141,46 @@ function buildOneMarkdown(meta, prevMeta, nextMeta, metas) {
   var outfile = meta.outfile;
   var ispost = prevMeta || nextMeta;
 
-  const pathToTop = '../'.repeat((filepath.split('/').length - 1));
-  const pathToCss = pathToTop + 'assets/modest.css';
+  const pathToTop = "../".repeat(filepath.split("/").length - 1);
+  const pathToCss = pathToTop + "assets/modest.css";
 
   var pandocParams = [
-    '--wrap=none', '--filter', 'filter.js', '--no-highlight', '-t', 'html5', '-f',
-    'gfm-hard_line_breaks+yaml_metadata_block'
+    "--wrap=none",
+    "--filter",
+    "filter.js",
+    "--no-highlight",
+    "-t",
+    "html5",
+    "-f",
+    "gfm-hard_line_breaks+yaml_metadata_block",
   ];
   const prep = preprocessMarkdown(meta.contents, meta);
-  var htmlPromise = spawnPromise(spawn('pandoc', pandocParams),
-                                 prep);
+  var htmlPromise = spawnPromise(spawn("pandoc", pandocParams), prep);
 
-  var headHtmlPromise = htmlPromise.then(async html => {
+  var headHtmlPromise = htmlPromise.then(async (html) => {
     if (meta.mathjax) {
-      html = html.replace(/\\&amp;/g, '&');
+      html = html.replace(/\\&amp;/g, "&");
     }
     var tohighlight = html.search(/<pre[^<]*<code/) >= 0;
     if (tohighlight) {
-      let window = (new JSDOM(html, {runScripts : "outside-only"})).window;
+      let window = new JSDOM(html, { runScripts: "outside-only" }).window;
       window.eval(hl);
-      window.eval(`hljs.initHighlighting();`)
+      window.eval(`hljs.initHighlighting();`);
       html = window.document.body.innerHTML;
     }
 
     var head = '<!doctype html>\n<head><meta charset="utf-8" />\n';
     head += `<title>${meta.title}</title>\n`;
-    head += `<link href="${
-        filepathToAbspath(
-            'atom.xml')}" type="application/atom+xml" rel="alternate" />\n`;
-    head += social(outfile, meta.title, meta.description,
-                   meta.socialBanner || meta.banner, meta.outfile);
+    head += `<link href="${filepathToAbspath(
+      "atom.xml"
+    )}" type="application/atom+xml" rel="alternate" />\n`;
+    head += social(
+      outfile,
+      meta.title,
+      meta.description,
+      meta.socialBanner || meta.banner,
+      meta.outfile
+    );
     head += `<link href="${pathToCss}" rel="stylesheet">`;
     if (tohighlight) {
       head += hlcss;
@@ -174,12 +194,11 @@ function buildOneMarkdown(meta, prevMeta, nextMeta, metas) {
       head += mathjaxPreamble;
     }
 
-    head += '\n</head>\n';
+    head += "\n</head>\n";
 
     head += meta.banner
-                ? (await banner(meta.banner,
-                                filepath.split('/').slice(0, -1).join('/')))
-                : '';
+      ? await banner(meta.banner, filepath.split("/").slice(0, -1).join("/"))
+      : "";
 
     if (ispost) {
       head += topnav();
@@ -191,22 +210,25 @@ function buildOneMarkdown(meta, prevMeta, nextMeta, metas) {
       head += subline(meta);
     }
 
-    return [ head, html ];
+    return [head, html];
   });
 
-  var postIndexP = filepath === 'index.md' ? metasTopostIndexPromise(metas)
-                                           : Promise.resolve('');
+  var postIndexP =
+    filepath === "index.md"
+      ? metasTopostIndexPromise(metas)
+      : Promise.resolve("");
   var footPromise = prevNextToFootPromise(prevMeta, nextMeta);
 
-  Promise.all([ headHtmlPromise, postIndexP, footPromise ])
-      .then(([ [ head, html ], postIndex, foothtml ]) => {
-        console.log('done ' + outfile);
-        writeFileAsync(outfile, '')
-            .then(() => appendFileAsync(outfile, head))
-            .then(() => appendFileAsync(outfile, postIndex))
-            .then(() => appendFileAsync(outfile, html))
-            .then(() => appendFileAsync(outfile, foothtml));
-      });
+  Promise.all([headHtmlPromise, postIndexP, footPromise]).then(
+    ([[head, html], postIndex, foothtml]) => {
+      console.log("done " + outfile);
+      writeFileAsync(outfile, "")
+        .then(() => appendFileAsync(outfile, head))
+        .then(() => appendFileAsync(outfile, postIndex))
+        .then(() => appendFileAsync(outfile, html))
+        .then(() => appendFileAsync(outfile, foothtml));
+    }
+  );
 }
 
 function prevNextToFootPromise(prevMeta, nextMeta) {
@@ -214,25 +236,29 @@ function prevNextToFootPromise(prevMeta, nextMeta) {
     var foot = `<p>
 <small>`;
     if (prevMeta) {
-      foot += `Previous: <a href="${
-        filepathToAbspath(prevMeta.outfile)}">${prevMeta.title}</a><br>`;
+      foot += `Previous: <a href="${filepathToAbspath(prevMeta.outfile)}">${
+        prevMeta.title
+      }</a><br>`;
     }
     if (nextMeta) {
-      foot +=
-          `Next: <a href="${filepathToAbspath(nextMeta.outfile)}">${nextMeta.title}</a>`;
+      foot += `Next: <a href="${filepathToAbspath(nextMeta.outfile)}">${
+        nextMeta.title
+      }</a>`;
     }
     foot += `</small>
-</p>`
+</p>`;
     return spawnPromise(
-        spawn('pandoc',
-              [
-                '--wrap=none',
-                '-t', 'html5', '-f',
-                'gfm-hard_line_breaks'
-              ]),
-        foot);
+      spawn("pandoc", [
+        "--wrap=none",
+        "-t",
+        "html5",
+        "-f",
+        "gfm-hard_line_breaks",
+      ]),
+      foot
+    );
   }
-  return Promise.resolve('');
+  return Promise.resolve("");
 }
 
 function shortDate(d) {
@@ -240,34 +266,39 @@ function shortDate(d) {
 }
 function metasTopostIndexPromise(metas) {
   var md =
-      '## All posts\n' +
-      metas
-          .map(meta =>
-                   `- [${meta.title}](${filepathToAbspath(meta.outfile)}) (${
-                       shortDate(meta.date)}, ${meta.tags.join('/')})\n`)
-          .join('');
-  md += `\n(<a href="${filepathToAbspath('atom.xml')}">Feed</a>)`
+    "## All posts\n" +
+    metas
+      .map(
+        (meta) =>
+          `- [${meta.title}](${filepathToAbspath(meta.outfile)}) (${shortDate(
+            meta.date
+          )}, ${meta.tags.join("/")})\n`
+      )
+      .join("");
+  md += `\n(<a href="${filepathToAbspath("atom.xml")}">Feed</a>)`;
   return spawnPromise(
-      spawn(
-          'pandoc',
-          [
-            '--wrap=none',
-            '-f',
-            'gfm-hard_line_breaks',
-            '-t', 'html5'
-          ]),
-      md);
+    spawn("pandoc", [
+      "--wrap=none",
+      "-f",
+      "gfm-hard_line_breaks",
+      "-t",
+      "html5",
+    ]),
+    md
+  );
 }
 
 function fileToMeta(filepath) {
-  var contents = fs.readFileSync(filepath, 'utf8');
-  var outfile = filepath.replace(/md$/, 'html');
-  var meta = Object.assign({}, defaultMeta, {filepath, outfile});
+  var contents = fs.readFileSync(filepath, "utf8");
+  var outfile = filepath.replace(/md$/, "html");
+  var meta = Object.assign({}, defaultMeta, { filepath, outfile });
   meta.contents = contents;
-  if (contents.indexOf('---\n') === 0) {
+  if (contents.indexOf("---\n") === 0) {
     meta = Object.assign(
-        {}, meta,
-        yaml.safeLoad(contents.substring(4, contents.indexOf('\n---\n', 4))));
+      {},
+      meta,
+      yaml.safeLoad(contents.substring(4, contents.indexOf("\n---\n", 4)))
+    );
     if (meta.date) {
       meta.date = new Date(meta.date);
     }
@@ -280,38 +311,40 @@ function fileToMeta(filepath) {
 
 function topnav() {
   return `  <ul class="top-nav">
-    <li><a href="${filepathToAbspath('')}">Blog</a></li>
-    <li><a href="${filepathToAbspath('#contact')}">Contact</a></li>
-    <li><a href="${filepathToAbspath('atom.xml')}">Feed</a></li>
+    <li><a href="${filepathToAbspath("")}">Blog</a></li>
+    <li><a href="${filepathToAbspath("#contact")}">Contact</a></li>
+    <li><a href="${filepathToAbspath("atom.xml")}">Feed</a></li>
   </ul>`;
-};
+}
 
 function subline(meta) {
-  var t = filepathToAbspath('');
+  var t = filepathToAbspath("");
   var text = ``;
-  var tagtext = meta.tags.map(s => `‘${s}’`).join('—');
-  if (meta.date && (meta.tags && meta.tags.length)) {
+  var tagtext = meta.tags.map((s) => `‘${s}’`).join("—");
+  if (meta.date && meta.tags && meta.tags.length) {
     text += `Updated on ${meta.date.toUTCString()}, tagged with ${tagtext}.`;
   } else if (meta.date) {
     text += `Updated on ${meta.date.toUTCString()}.`;
   } else if (meta.tags && meta.tags.length) {
-    text += `Tagged with ${tagtext}.`
+    text += `Tagged with ${tagtext}.`;
   }
   return `<p><em>${text}</em></p>`;
 }
 
 async function banner(url, parentPath) {
-  const [w, h] = await imageToSize((parentPath + '/' + url).replace(/^\//, ''));
-  return `<figure class="full-width no-top"><img class="top-banner-image" src="${
-      url}" width="${w}" height="${h}"></figure>`;
+  const [w, h] = await imageToSize((parentPath + "/" + url).replace(/^\//, ""));
+  return `<figure class="full-width no-top"><img class="top-banner-image" src="${url}" width="${w}" height="${h}"></figure>`;
 }
 
 function imageToSize(imagepath) {
-  return spawnPromise(spawn('identify', [ imagepath ]))
-      .then(s => s.split(/\s+/)[2].split('x'));
+  return spawnPromise(spawn("identify", [imagepath])).then((s) =>
+    s.split(/\s+/)[2].split("x")
+  );
 }
 
-function headline(title) { return `<h1>${title}</h1>`; }
+function headline(title) {
+  return `<h1>${title}</h1>`;
+}
 
 function social(url, title, description, banner, outfile) {
   return `<meta name="description" content="${description}" />
